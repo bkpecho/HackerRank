fa63b6c7cc2fd21d2eff546c1425003c913f74fa
BK Pecho <bkpecho05@gmail.com>
2023-03-12 11:07:40 +0800

    Update aVeryBigSum

--- aVeryBigSum.js
+++ aVeryBigSum.js
@@ -1,5 +1,7 @@
 /* 
 
+Challenge Link: https://www.hackerrank.com/challenges/a-very-big-sum/problem
+
 In this challenge, you are required to calculate and print the sum of the elements in an array, keeping in mind that some of those integers may be quite large.
 
 Function Description
