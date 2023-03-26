1ffc4293c6bb46288562dd0cc1f151e3d07e2c1a
BK Pecho <bkpecho05@gmail.com>
2023-03-26 08:56:50 +0800

    plusMinus

---
+++ plusMinus.js
@@ -0,0 +1,52 @@
+/* 
+
+Challenge link = https://www.hackerrank.com/challenges/plus-minus/problem
+
+*/
+
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function (inputStdin) {
+  inputString += inputStdin;
+});
+
+process.stdin.on('end', function () {
+  inputString = inputString.split('\n');
+
+  main();
+});
+
+function readLine() {
+  return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'plusMinus' function below.
+ *
+ * The function accepts INTEGER_ARRAY arr as parameter.
+ */
+
+function plusMinus(arr) {
+  // Write your code here
+  let count = [0, 0, 0];
+
+  arr.forEach((x) => (x > 0 ? count[0]++ : x < 0 ? count[1]++ : count[2]++));
+  return count.forEach((x) => console.log((x / arr.length).toFixed(6)));
+}
+
+function main() {
+  const n = parseInt(readLine().trim(), 10);
+
+  const arr = readLine()
+    .replace(/\s+$/g, '')
+    .split(' ')
+    .map((arrTemp) => parseInt(arrTemp, 10));
+
+  plusMinus(arr);
+}
