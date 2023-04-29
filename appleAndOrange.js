7b5b624e16fd9ef2cf07825ea6859363d0efc74a
Bryan King Pecho <bkpecho05@gmail.com>
2023-04-29 22:08:34 +0800

    appleAndOrange

---
+++ appleAndOrange.js
@@ -0,0 +1,78 @@
+/*
+
+Problem Link: https://www.hackerrank.com/challenges/apple-and-orange/problem
+
+*/
+
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'countApplesAndOranges' function below.
+ *
+ * The function accepts following parameters:
+ *  1. INTEGER s
+ *  2. INTEGER t
+ *  3. INTEGER a
+ *  4. INTEGER b
+ *  5. INTEGER_ARRAY apples
+ *  6. INTEGER_ARRAY oranges
+ */
+
+function countApplesAndOranges(s, t, a, b, apples, oranges) {
+    
+    let check_apples = apples.map(x=> a + x)
+    let check_oranges = oranges.map(x=> b + x)
+    
+    let total_apples = check_apples.filter(x=> x >= s && x <= t)
+    let total_oranges = check_oranges.filter(x=> x >= s && x <= t)
+    
+    console.log(total_apples.length)
+    console.log(total_oranges.length)
+
+}
+
+function main() {
+    const firstMultipleInput = readLine().replace(/\s+$/g, '').split(' ');
+
+    const s = parseInt(firstMultipleInput[0], 10);
+
+    const t = parseInt(firstMultipleInput[1], 10);
+
+    const secondMultipleInput = readLine().replace(/\s+$/g, '').split(' ');
+
+    const a = parseInt(secondMultipleInput[0], 10);
+
+    const b = parseInt(secondMultipleInput[1], 10);
+
+    const thirdMultipleInput = readLine().replace(/\s+$/g, '').split(' ');
+
+    const m = parseInt(thirdMultipleInput[0], 10);
+
+    const n = parseInt(thirdMultipleInput[1], 10);
+
+    const apples = readLine().replace(/\s+$/g, '').split(' ').map(applesTemp => parseInt(applesTemp, 10));
+
+    const oranges = readLine().replace(/\s+$/g, '').split(' ').map(orangesTemp => parseInt(orangesTemp, 10));
+
+    countApplesAndOranges(s, t, a, b, apples, oranges);
+}
